8375c2919346477657e3ea08c74473faf6898eae
John Scott <johnrscott0@gmail.com>
2021-07-03 14:11:53 +0100

    Added another test

--- src/tests/basic-tests.cpp
+++ src/tests/basic-tests.cpp
@@ -7,6 +7,26 @@
 #include <qsl/utils/misc.hpp>
 #include <qsl/qubits.hpp>
 
+TEST_CASE( "Qubits object copy constructor", "[constructors]" ) {
+
+    const unsigned num_qubits{ 5 };
+    qsl::Qubits<qsl::Type::Default, double> q{ num_qubits };
+
+    // Set q to a random state
+    q.setState(qsl::makeRandomState<double>(num_qubits));
+
+    // Construct another object using the copy constructor
+    qsl::Qubits<qsl::Type::Default, double> q_copy{ q };    
+
+    // Check that the other properties are also the same
+    REQUIRE(q.getNumQubits() == q_copy.getNumQubits());
+    
+    // Check that both objects have the same internal state
+    double distance = qsl::distance(q.getState(), q_copy.getState());
+    REQUIRE(std::abs(distance) < 1e-10);
+
+}
+
 TEST_CASE( "Test Fubini Study distance", "[quantum-utils]" ) {
 
     std::vector<qsl::complex<double>> a{ {1,0}, {0,1}, {1,1} }; 
