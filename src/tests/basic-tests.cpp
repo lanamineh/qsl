72c84fff2fed898321b50e6c91d39599120fbf5e
John Scott <johnrscott0@gmail.com>
2021-07-03 14:46:19 +0100

    Added a few more tests to test code coverage generation

--- src/tests/basic-tests.cpp
+++ src/tests/basic-tests.cpp
@@ -7,6 +7,51 @@
 #include <qsl/utils/misc.hpp>
 #include <qsl/qubits.hpp>
 
+TEST_CASE ("Test the innerProduct function", "[quantum-utils]")
+{
+    const std::vector<qsl::complex<double>> a{ {1,1.2}, {1.2,1}, {1,1.5} };
+    const std::vector<qsl::complex<double>> b{ {1,0}, {0,-1}, {-1,0} };
+    const std::vector<qsl::complex<double>> c{
+	{1.2,0}, {1.2,-2.1}, {1.3,2}, {0,0} };
+
+    // Check that inner product throws exception for different sized vectors
+    CHECK_THROWS(innerProduct(a,c));	 
+		 
+}
+
+
+TEST_CASE ("Test the checkStateSize function", "[quantum-utils]")
+{
+    // Not a valid state size, should throw exception
+    const std::vector<qsl::complex<double>> pretend_state{ {1,0}, {0,1}, {1,1} };
+    CHECK_THROWS(checkStateSize(pretend_state));
+
+    // Check that the function returns the correct state size
+    const std::vector<qsl::complex<double>> state{
+	{1,0}, {0,1}, {1,0}, {0,1} };
+
+    REQUIRE( checkStateSize(state) == 2 );
+}
+
+TEST_CASE( "Test Fubini Study distance", "[quantum-utils]" )
+{
+    std::vector<qsl::complex<double>> a{ {1,0}, {0,1}, {1,1} }; 
+    std::vector<qsl::complex<double>> double_a{ {2,0}, {0,2}, {2,2} }; 
+
+    std::vector<qsl::complex<double>> b{ {1.3,2}, {1,1.3}, {1.5,0.2} }; 
+    std::vector<qsl::complex<double>> double_b{ {2.6,4}, {2,2.6}, {3,0.4} }; 
+
+    // Test that distance between equal and scaled vectors is zero
+    REQUIRE( fubiniStudy(a,a) < 1e-13);
+    REQUIRE( fubiniStudy(double_a,a) < 1e-13);
+
+    double distance_a_b = fubiniStudy(a,b);
+    double scaled_distance_a_b = fubiniStudy(a,double_b);
+
+    REQUIRE( std::abs(distance_a_b - scaled_distance_a_b) < 1e-13);
+    
+}
+
 TEST_CASE( "Qubits object copy constructor", "[constructors]" ) {
 
     const unsigned num_qubits{ 5 };
@@ -25,10 +70,4 @@ TEST_CASE( "Qubits object copy constructor", "[constructors]" ) {
     double distance = qsl::distance(q.getState(), q_copy.getState());
     REQUIRE(std::abs(distance) < 1e-10);
 
-}
-
-TEST_CASE( "Test Fubini Study distance", "[quantum-utils]" ) {
-
-    std::vector<qsl::complex<double>> a{ {1,0}, {0,1}, {1,1} }; 
-    REQUIRE( fubiniStudy(a,a) < 1e-13);
 }
