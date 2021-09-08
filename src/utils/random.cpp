4950312d3139998570ebb6f36f5fb6ffe9275628
John Scott <johnrscott0@gmail.com>
2021-09-08 20:23:57 +0100

    Updated copyright

--- src/utils/random.cpp
+++ src/utils/random.cpp
@@ -1,5 +1,6 @@
 /*
- *  Copyright 2021 Lana Mineh and John Scott
+ *  Authors: Lana Mineh and John Scott
+ *  Copyright 2021 Phasecraft Ltd. and John Scott
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
