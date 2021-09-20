9b419bb1a65d6410a41f63fc331f6a8c02f1b652
lanamineh <33867817+lanamineh@users.noreply.github.com>
2021-09-20 14:24:33 +0100

    Changed number of qubits

--- other/tests/make.sh
+++ other/tests/make.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DNUM_QUBITS=12 -DTEST_LEN=20000
+cmake .. -DCMAKE_BUILD_TYPE=Release -DNUM_QUBITS=16 -DTEST_LEN=1000
 cmake --build . -j8
 echo -e "\nFinished. Binaries are in build/bin/tests/"
 echo -e "\nstd::complex test"
