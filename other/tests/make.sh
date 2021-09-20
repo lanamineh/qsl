ef9fd87cf7ecc5c6053060c5b265502d902bb5d3
lanamineh <33867817+lanamineh@users.noreply.github.com>
2021-09-20 13:22:05 +0100

    Changed tests makefile

--- other/tests/make.sh
+++ other/tests/make.sh
@@ -1,5 +1,13 @@
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DNUM_QUBITS=12 -DTEST_LEN=4
+cmake .. -DCMAKE_BUILD_TYPE=Release -DNUM_QUBITS=12 -DTEST_LEN=20000
 cmake --build . -j8
 echo -e "\nFinished. Binaries are in build/bin/tests/"
+echo -e "\nstd::complex test"
+./bin/tests/stdcmplx_test
+echo -e "\nstruct test"
+./bin/tests/struct_test
+echo -e "\nflat test"
+./bin/tests/flat_test
+echo -e "\nseparate test"
+./bin/tests/separate_test
