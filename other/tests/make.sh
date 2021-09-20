da11def12c6dc95555680807e24469aac4bdd006
John Scott <johnrscott0@gmail.com>
2021-09-20 13:17:31 +0100

    Fixed cmake file to read variables in statevec-tests

--- other/tests/make.sh
+++ other/tests/make.sh
@@ -1,6 +1,5 @@
-rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DNUM_QUBITS=12 -DTEST_LEN=20000
+cmake .. -DCMAKE_BUILD_TYPE=Release -DNUM_QUBITS=12 -DTEST_LEN=4
 cmake --build . -j8
 echo -e "\nFinished. Binaries are in build/bin/tests/"
