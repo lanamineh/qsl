b8f1c2f0506bd1263eb3e5129dc9ab3c473aa529
John Scott <johnrscott0@gmail.com>
2021-10-11 15:49:18 +0100

    Modified the benchmark file

--- examples/benchmark.cpp
+++ examples/benchmark.cpp
@@ -4,29 +4,28 @@
 
 int main ()
 {
-    /*
     unsigned nqubits = 3;
     qsl::Qubits<qsl::Type::Default, double> q{nqubits};
 
     qsl::Compare<qsl::Test::SingleSim,
-		 qsl::Qubits<qsl::Type::Default, double>,
-		 qsl::Qubits<qsl::Type::Omp, double>> cmp{16,100};
+		 qsl::Qubits<qsl::Type::Omp, double>,
+		 qsl::Qubits<qsl::Type::Default, double>> cmp{16,100};
     cmp.phase();
     cmp.pauliX();
     cmp.rotateX();
     cmp.controlNot();
-    */
-    using Sim1 = qsl::Qubits<qsl::Type::Default, double>;
+
+    // using Sim1 = qsl::Qubits<qsl::Type::Default, double>;
    
-    qsl::Time<qsl::Test::MultiSim, Sim1, qsl::Restrictions::None> time(12, 200, 100);
-    qsl::Results res1 = time.pauliX();
-    res1.writeToFile("pauliX.csv");
-    qsl::Results res2 = time.rotateX();
-    res2.writeToFile("rotateX.csv");
-    qsl::Results res3 = time.controlNot();
-    res3.writeToFile("cNot.csv");
-    qsl::Results res4 = time.controlPhase();
-    res4.writeToFile("cPhaseShift.csv");
+    // qsl::Time<qsl::Test::MultiSim, Sim1, qsl::Restrictions::None> time(12, 200, 100);
+    // qsl::Results res1 = time.pauliX();
+    // //res1.writeToFile("pauliX.csv");
+    // qsl::Results res2 = time.rotateX();
+    // //res2.writeToFile("rotateX.csv");
+    // qsl::Results res3 = time.controlNot();
+    // //res3.writeToFile("cNot.csv");
+    // qsl::Results res4 = time.controlPhase();
+    // //res4.writeToFile("cPhaseShift.csv");
 
     
 }
