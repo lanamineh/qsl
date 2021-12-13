098a021bbece5e9f9b148a1dccaf0f613ccd40aa
John Scott <johnrscott0@gmail.com>
2021-12-13 00:50:10 +0000

    Been playing around with variadic inheritance and default values

--- v1/draft2/generic.cpp
+++ v1/draft2/generic.cpp
@@ -1,6 +1,20 @@
 #include <vector>
 #include <iostream>
+#include <tuple>
 
+class A_def
+{
+public:
+    int a = 3;
+    A_def() { std::cout << "A_def" << std::endl; }
+};
+
+class B_def
+{
+public:
+    int b = 4;
+    B_def() { std::cout << "B_def" << std::endl; }    
+};
 
 class A
 {
@@ -16,15 +30,16 @@ public:
     B() { std::cout << "B" << std::endl; }    
 };
 
-template<typename... Args>
-class C : private Args...
+template<std::floating_point Fp, typename... Args>
+class C : public Args...
 {
+    
     
 };
 
 int main()
 {
-    C<A,B> c;
-    std::cout << c.a << std::endl;
-    std::cout << c.b << std::endl;
+    C<double, A, B> c;
+    //std::cout << c.a << std::endl;
+    //std::cout << c.b << std::endl;
 }
