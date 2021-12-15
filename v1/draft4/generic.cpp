6025b063371f306c14f0611e2a973b54c7c2cfea
John Scott <johnrscott0@gmail.com>
2021-12-15 14:48:20 +0000

    Started building up draft4

--- v1/draft4/generic.cpp
+++ v1/draft4/generic.cpp
@@ -1,1 +1,23 @@
-int main() {}
+#include <iostream>
+
+template<typename... Args> struct Typelist;
+
+template<>
+struct Typelist<>
+{
+    static void print() { std::cout << std::endl; };
+};
+
+template<typename First, typename... Rest>
+struct Typelist<First, Rest...>
+{
+    static void print() {
+	std::cout << typeid(First).name() << ",";
+	Typelist<Rest...>::print();
+    }
+};
+
+int main()
+{
+    Typelist<int,double,double,unsigned>::print();
+}
