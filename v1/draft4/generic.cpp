3b585f3531bdfcd72e31ba22f34ebae6d8883678
John Scott <johnrscott0@gmail.com>
2021-12-15 16:47:08 +0000

    Added a size variable using partial specialisation

--- v1/draft4/generic.cpp
+++ v1/draft4/generic.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 
 template<typename... Args>
-struct Typelist
+struct PrintFn
 {
     /**
-     * \brief Print the types
-     *
+     * \brief Print the types in Args...
      */
-    static void print()
+    static constexpr void print()
 	{
 	    ((std::cout << typeid(Args).name() << ","), ...);
 	    std::cout << std::endl;
 	}
 };
 
+template<typename... Args> struct Typelist;
+
+template<>
+struct Typelist<> : PrintFn<>
+{
+    static constexpr std::size_t size = 0;
+};
+
+template<typename First, typename... Rest>
+struct Typelist<First, Rest...> : PrintFn<First, Rest...>
+{
+    static constexpr std::size_t size = Typelist<Rest...>::size + 1;
+};
+
 int main()
 {
-    Typelist<>::print();
-
+    std::cout << Typelist<int,double,void>::size << std::endl;
+    Typelist<int,double,void*>::print();
 }
