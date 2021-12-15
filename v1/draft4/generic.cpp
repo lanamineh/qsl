8e61db6c1e80f2cfc7ae744dd5019a977d33b94e
John Scott <johnrscott0@gmail.com>
2021-12-15 14:59:18 +0000

    Added a simple print using fold expressions

--- v1/draft4/generic.cpp
+++ v1/draft4/generic.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 
-template<typename... Args> struct Typelist;
-
-template<>
-struct Typelist<>
+template<typename... Args>
+struct Typelist
 {
-    static void print() { std::cout << std::endl; };
-};
-
-template<typename First, typename... Rest>
-struct Typelist<First, Rest...>
-{
-    static void print() {
-	std::cout << typeid(First).name() << ",";
-	Typelist<Rest...>::print();
-    }
+    /**
+     * \brief Print the types
+     *
+     */
+    static void print()
+	{
+	    ((std::cout << typeid(Args).name() << ","), ...);
+	    std::cout << std::endl;
+	}
 };
 
 int main()
 {
-    Typelist<int,double,double,unsigned>::print();
+    Typelist<>::print();
+
 }
