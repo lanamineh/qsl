704201ada0e7042906f3cdde1ad1b80805cace05
John Scott <johnrscott0@gmail.com>
2022-04-10 12:36:34 +0100

    Found problem in make_random prototype

--- v1/header/test.cpp
+++ v1/header/test.cpp
@@ -9,9 +9,11 @@
 
 #include "qsl.hpp"
 #include <vector>
+#include <iostream>
 
 int main()
 {
+    // Constructors
     qsl::basic<double> q1{3}; // A simple qsl simulator containing three qubits
     qsl::resize<double, true> q2{5}; // A resizeable simulator of 5 qubits, with debugging
     qsl::number<double, false, qsl::seq> q3{2}; // Two qubit sequential fixed-number simulator
@@ -19,7 +21,8 @@ int main()
     //qsl::basic<float> q4{{1,0},{1,0}}; // You cannot implicitly initialise a std::vector
     std::vector<std::complex<float>> s1{{1,0},{1,0}}; // |+) state
     qsl::basic<float> q4{s1}; // You can initialise explicitly from a std::vector    
-    
+
+    // Gates
     for (unsigned n{0}; n < q1.size(); ++n) {
 	q1.h(n); // Apply a Hadamard gate to all qubits in q1
     }
@@ -27,12 +30,24 @@ int main()
     q3.z(1); // Apply a Pauli Z gate to qubit 1
     q1.rx(0, 0.3); // Apply an Rx rotation of 0.3 radians to qubit 0
 
+    // Basic functions
     [[maybe_unused]] std::size_t d{q1.dim()}; // Get the dimension of the state vector
-
+    unsigned n{q2.size()}; // Get the number of qubits
+    n = q3.get_ones(); // Get the number of ones in a fixed-number simulator
+    
     std::vector<std::complex<double>> s2{{1,0},{0,0},{0,0},{1,0}};
     q2.set_state(s2); // You can set the state to an arbitrary number of qubits
     // q4.set_state(s2); // You cannot implicitly change the floating point type
+    q3.make_random();
     
+    //TODO
+    //std::cout << q1 << std::endl; // Send any simulator to an ostream
+    //std::cout << q2 << std::endl;
+    //std::cerr << q3 << std::endl;
+    //std::cerr << q4 << std::endl;
+    q2.print(); // Print directly to console output (with a trailing newline)
+    q4.print(std::cerr); // Pass a custom output stream
+
     
     
 }
