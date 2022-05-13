af463fe5cc29143f160821982812705981c12318
lanamineh <33867817+lanamineh@users.noreply.github.com>
2022-05-13 22:32:56 +0100

    Got combinations of tuple templates working

--- v1/experiments/gtest/sim-tests.cpp
+++ v1/experiments/gtest/sim-tests.cpp
@@ -120,20 +120,20 @@ struct make_case
                       typename std::tuple_element<I % N, Tuple2>::type>;
 };
 
-template <class T1, class T2, class Is>
+template <class Is, class T1, class... T2>
 struct make_combinations;
 
 template <class Tuple1, class Tuple2, std::size_t... Is>
-struct make_combinations<Tuple1, Tuple2, std::index_sequence<Is...>>
+struct make_combinations<std::index_sequence<Is...>, Tuple1, Tuple2>
 {
     using tuples = std::tuple<typename make_case<Tuple1, Tuple2, Is>::type...>;
 };
 
 template<class Tuple1, class Tuple2>
 using Combinations_t = typename make_combinations
-                       <Tuple1, Tuple2,
-			std::make_index_sequence<
-			    (std::tuple_size<Tuple1>::value)*(std::tuple_size<Tuple2>::value)>
+                       <std::make_index_sequence<
+			    (std::tuple_size<Tuple1>::value)*(std::tuple_size<Tuple2>::value)>,
+			Tuple1, Tuple2
 			>::tuples;
 
 template <class T>
@@ -168,3 +168,88 @@ TYPED_TEST(MyTestFixture, Test12)
     EXPECT_EQ(s1.state.size(), 4);
 
 }
+
+
+//////////////////////////////////////////////////////////////////
+template <typename T>
+class MyTestFixture2 : public testing::Test {};
+
+// Tuple concatenation
+template <typename T, typename...>
+struct cat
+{
+    using type = T;
+};
+
+template <typename... T1, typename... T2, typename... T3>
+struct cat<std::tuple<T1...>, std::tuple<T2...>, T3...>
+{
+    using type = cat<std::tuple<T1..., T2...>, T3...>::type;
+};
+
+template <typename... T>
+using cat_t = cat<T...>::type;
+
+
+// Prepend a type to the beginning of every tuple in a list of tuples
+template <typename T, typename V>
+struct prepend{};
+
+// Takes std::tuple<std::tuples> as input
+template <typename T, typename... L>
+struct prepend<T, std::tuple<L...>>
+{
+    using type = std::tuple<cat_t<std::tuple<T>, L>...>;
+};
+
+template <typename T, typename V>
+using prepend_t = prepend<T, V>::type;
+
+
+// Combinations
+template <typename... T>
+struct comb {};
+
+template <typename... T>
+struct comb<std::tuple<T...>>
+{
+    using type = std::tuple<std::tuple<T>...>;
+};
+
+template <typename... T1, typename... T2>
+struct comb<std::tuple<T1...>, T2...>
+{
+    using combined = comb<T2...>::type;
+
+    using type = cat_t<prepend_t<T1, combined>...>;
+};
+
+template <typename... T>
+using comb_t = comb<T...>::type;
+
+
+
+using TestTypes2 = ::testing::Types<cat_t<std::tuple<double>,
+					  std::tuple<float, long double>,
+					  std::tuple<int>>>;
+
+//using TestTypes3 = ::testing::Types<head_t<std::tuple<double, float, long double>>>;
+
+using TestTypes4 = Test<comb_t<std::tuple<double, char>,
+			       std::tuple<float, long double>,
+			       std::tuple<int, unsigned>>>::Types;
+
+//using TestTypes4 = ::testing::Types<comb_t<std::tuple<float, int>, std::tuple<double, char>>>;
+
+
+using TestTypes5 = ::testing::Types<append_t<double, std::tuple<std::tuple<float>, std::tuple<int>>>>;
+
+
+TYPED_TEST_SUITE(MyTestFixture2, TestTypes4);
+
+TYPED_TEST(MyTestFixture2, Test12)
+{
+    EXPECT_EQ(0, 0);
+}
+
+
